1cf0b95efff9b1045d07a93ef1f055e6f00a1ab7
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-30 13:25:51 +0100

    finish first part of exercise

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@ Stampare a schermo attraverso il for in tutte le proprietà.
 - Creare un array di oggetti di studenti.
 Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
 - Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. */
+//@ts-check
+
+
+/* Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età.
+Stampare a schermo attraverso il for in tutte le proprietà */
+
+var student = {
+    "nome" : "marco",
+    "cognome": "rispoli",
+    "eta" : 28
+}
+
+for(var dati in student){
+    console.log("lo studente è " + dati + ",i valori sono " + student[dati]);
+}
