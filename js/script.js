f42e57c66dc2bdd90e4c96cc6109f2a96b44b9b9
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-30 13:44:29 +0100

    finish second part of exercise

--- js/script.js
+++ js/script.js
@@ -17,5 +17,41 @@ var student = {
 }
 
 for(var dati in student){
-    console.log("lo studente è " + dati + ",i valori sono " + student[dati]);
+    console.log("lo studente è " + dati + ": " + student[dati]);
 }
+
+/* Creare un array di oggetti di studenti.
+Ciclare su tutti gli studenti e stampare per ognuno nome e cognome */
+
+var studenti = [
+    { 
+        "nome" : "fabio",
+        "cognome": "mancini",
+        "eta": 30
+
+    },
+    {
+        "nome": "marco",
+        "cognome": "salvi",
+        "eta": 26
+
+    },
+    {
+        "nome": "elena",
+        "cognome": "fierro",
+        "eta": 25
+
+    }
+]
+
+
+    for(var primoStudente in studenti[0]){
+        console.log("lo studente è " + primoStudente + ": " + studenti[0][primoStudente]);
+    }
+    for(var primoStudente in studenti[1]){
+        console.log("lo studente è " + primoStudente + ": " + studenti[1][primoStudente]);
+    }
+    for(var primoStudente in studenti[2]){
+
+        console.log("lo studente è " + primoStudente + ":" + studenti[2][primoStudente]);
+    }
